6f0eb7b843dbc26dab7cb4ee829184cec66db428
ashhh97 <ashleyz10969@gmail.com>
2022-05-27 00:02:44 -0400

    tabs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const navSlide = () =>{
+/*const navSlide = () =>{
   const lines = document.querySelector('.lines');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
@@ -12,7 +12,28 @@ const navSlide = () =>{
   });
 }
 
-navSlide();
+navSlide();*/
 
 
 
+const tabs = document.querySelectorAll(".tabs li"); //  matches a CSS selector(s).
+const contents = document.querySelectorAll(".content"); 
+
+/*
+arrow function
+before:
+  hello = function(){
+   return "hello world";
+  }
+after:
+  hello = ()=>{
+    return "hello world";
+  }
+*/
+
+//add a click even to a <button> element
+tabs.forEach((tab) => {
+  tab.addEventListener("click",() =>{
+    tab.classList.add("active");
+  }); 
+});
