c256381c834a89d61f64965d6323fb9fa8df97f7
ashhh97 <ashleyz10969@gmail.com>
2022-05-28 18:03:30 -0400

    tabs

--- script.js
+++ script.js
@@ -17,7 +17,26 @@ navSlide();*/
 
 
 const tabs = document.querySelectorAll(".tabs li"); //  matches a CSS selector(s).
+/*console.log(tabs);*/
 const contents = document.querySelectorAll(".content"); 
+/*console.log(contents);*/
+tabs.forEach((tab,index)=>{
+  tab.addEventListener('click',()=> {
+    // to remove active class from previous tab
+    tabs.forEach(tab=>tab.classList.remove('active'));
+
+    tab.classList.add('active');
+    //to show content according to tab slected
+    //to hide previous tab content
+    contents.forEach(c => c.classList.remove("active"));
+    contents[index].classList.add("active");
+    
+
+  });
+});
+
+// to run the animation initially when the oage loads
+tabs[0].click();
 
 /*
 arrow function
@@ -31,9 +50,3 @@ after:
   }
 */
 
-//add a click even to a <button> element
-tabs.forEach((tab) => {
-  tab.addEventListener("click",() =>{
-    tab.classList.add("active");
-  }); 
-});
